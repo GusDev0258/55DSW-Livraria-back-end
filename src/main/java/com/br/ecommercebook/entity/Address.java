23fd883a4201585498afce65c0c6e4a161378de6
LuizFJP <luizfjportela@gmail.com>
2023-08-10 21:43:23 -0300

    fix address entity

--- src/main/java/com/br/ecommercebook/entity/Address.java
+++ src/main/java/com/br/ecommercebook/entity/Address.java
@@ -27,27 +27,10 @@ public class Address {
     @Column(name = "main_address", nullable = false)
     private boolean mainAddress;
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "address")
-    @JsonManagedReference
-    private List<Device> devices;
-
-    @OneToMany(cascade = CascadeType.ALL, mappedBy="address")
-    @JsonManagedReference
-    private List<EnergyBill> energyBills;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     @JsonBackReference
     private User user;
-
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "address")
-    @Column(name = "consumption_alerts")
-    @JsonManagedReference
-    private List<ConsumptionAlert> consumptionAlerts;
-
-    @ManyToOne
-    @JoinColumn(name = "energy_provider", nullable = true)
-    @JsonBackReference
-    private EnergyProvider energyProvider;
 
 }
