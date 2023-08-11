a04bbc306e4e90b76a71184c8471c4bd698148bf
LuizFJP <luizfjportela@gmail.com>
2023-08-10 21:10:12 -0300

    starting project

---
+++ src/test/java/com/br/ecommercebook/EcommerceBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ecommercebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
