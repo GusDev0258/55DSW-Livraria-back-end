6195612c2cf777f375dd379d7b6cf0cb32365672
LuizFJP <luizfjportela@gmail.com>
2023-08-11 21:42:36 -0300

    add models

--- src/main/java/com/br/ecommercebook/entity/Stock.java
+++ src/main/java/com/br/ecommercebook/entity/Stock.java
@@ -1,4 +1,21 @@
 package com.br.ecommercebook.entity;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "stock")
 public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "sotck")
+    @JsonManagedReference
+    private List<Book> book;
 }
--- src/main/java/com/br/ecommercebook/entity/SubCategory.java
+++ src/main/java/com/br/ecommercebook/entity/SubCategory.java
@@ -1,4 +1,21 @@
 package com.br.ecommercebook.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "sub_category")
 public class SubCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    @ManyToOne
+    @JoinColumn(name="category_id", nullable=false)
+    @JsonBackReference
+    private Category category;
 }
