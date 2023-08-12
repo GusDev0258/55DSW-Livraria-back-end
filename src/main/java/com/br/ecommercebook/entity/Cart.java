f3e1ad4522ceb1771235b6ba04582d0fc9d6091e
gusdev0258 <16326176760@edu.udesc.br>
2023-08-11 21:42:42 -0300

    feat: structuring entities

--- src/main/java/com/br/ecommercebook/entity/Cart.java
+++ src/main/java/com/br/ecommercebook/entity/Cart.java
@@ -1,4 +1,34 @@
 package com.br.ecommercebook.entity;
 
+import java.util.List;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "cart")
 public class Cart {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @ManyToMany(mappedBy="carts")
+  private List<Book> books;
+
+  @OneToOne
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+  
 }
--- src/main/java/com/br/ecommercebook/entity/CreditCard.java
+++ src/main/java/com/br/ecommercebook/entity/CreditCard.java
@@ -1,4 +1,32 @@
 package com.br.ecommercebook.entity;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "credit_card")
 public class CreditCard {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  private String number;
+  private String responsable_name;
+  private LocalDate expiration_date;
+  private String security_code;
+
+  @ManyToMany(mappedBy="credit_cards")
+  private List<User> users;
 }
