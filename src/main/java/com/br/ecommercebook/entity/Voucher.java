4e5f25257b206aa51dc34f72cb34eab45b0341c2
LuizFJP <luizfjportela@gmail.com>
2023-08-11 22:03:57 -0300

    add models

--- src/main/java/com/br/ecommercebook/entity/Voucher.java
+++ src/main/java/com/br/ecommercebook/entity/Voucher.java
@@ -1,4 +1,27 @@
 package com.br.ecommercebook.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "voucher")
 public class Voucher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private float discount;
+    private boolean active;
+    @Column(unique = true)
+    private int cod;
+    @ManyToMany(mappedBy="voucher")
+    private List<SubCategory> subCategory;
+    private LocalDateTime creationDate;
+    private LocalDateTime expiredDate;
 }
