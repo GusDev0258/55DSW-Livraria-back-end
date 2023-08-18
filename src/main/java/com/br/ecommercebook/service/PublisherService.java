8768554e654e4fcf592279324145bdfcf8aac4d8
gusdev0258 <16326176760@edu.udesc.br>
2023-08-18 20:56:37 -0300

    update pom

--- src/main/java/com/br/ecommercebook/service/PublisherService.java
+++ src/main/java/com/br/ecommercebook/service/PublisherService.java
@@ -1,4 +1,21 @@
 package com.br.ecommercebook.service;
 
+import org.springframework.stereotype.Service;
+
+import com.br.ecommercebook.dto.PublisherDTO;
+import com.br.ecommercebook.repository.PublisherRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
 public class PublisherService {
+
+  private final PublisherRepository publisherRepository;
+
+  
+  public Long create(PublisherDTO publisherDTO) {
+    var publisher = this.mapper.map(publisherDTO. Publisher.class);
+  }
+
 }
