0eb0a2c8b44f73aeb1415db6fc0ddf06e9e81ffd
Nicolas D <78985734+Deb4cker@users.noreply.github.com>
2023-08-19 20:05:12 -0300

    getCart

--- src/main/java/com/br/ecommercebook/service/CartService.java
+++ src/main/java/com/br/ecommercebook/service/CartService.java
@@ -1,4 +1,24 @@
 package com.br.ecommercebook.service;
 
+import com.br.ecommercebook.entity.Cart;
+import com.br.ecommercebook.repository.CartRepository;
+import com.br.ecommercebook.vo.CartVO;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+
 public class CartService {
+
+    private final ModelMapper modelMapper;
+    private final CartRepository repository;
+
+    public CartVO GetCartByUserId (Long userId){
+        var cart = repository.findCartByUserId(userId);
+        return modelMapper.map(cart, CartVO.class);
+    }
+
+
 }
--- src/main/java/com/br/ecommercebook/vo/CartVO.java
+++ src/main/java/com/br/ecommercebook/vo/CartVO.java
@@ -1,4 +1,10 @@
 package com.br.ecommercebook.vo;
 
+import lombok.Data;
+import java.util.List;
+
+@Data
 public class CartVO {
+    private long id;
+    private List<BookVO> books;
 }
