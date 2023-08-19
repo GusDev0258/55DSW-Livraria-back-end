85c2f93c7031fc580d3da74ee0b233c062f2c716
Nicolas D <78985734+Deb4cker@users.noreply.github.com>
2023-08-19 20:05:12 -0300

    getCart

--- src/main/java/com/br/ecommercebook/controller/CartController.java
+++ src/main/java/com/br/ecommercebook/controller/CartController.java
@@ -1,4 +1,24 @@
 package com.br.ecommercebook.controller;
 
+import com.br.ecommercebook.service.CartService;
+import com.br.ecommercebook.vo.CartVO;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@NoArgsConstructor
+@RequestMapping("api/cart")
 public class CartController {
+
+    private CartService service;
+    @GetMapping("/{userId}")
+    public ResponseEntity<CartVO> getUserCart(@PathVariable Long userId){
+        var response = service.GetCartByUserId(userId);
+        return new ResponseEntity(response, HttpStatus.OK);
+    }
 }
