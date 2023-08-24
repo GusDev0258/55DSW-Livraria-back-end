7574a23369d33e98a1612a562242d67bd6f72c1c
LuizFJP <luizfjportela@gmail.com>
2023-08-24 17:14:20 -0300

    feat category

--- src/main/java/com/br/ecommercebook/service/CategoryService.java
+++ src/main/java/com/br/ecommercebook/service/CategoryService.java
@@ -2,6 +2,8 @@ package com.br.ecommercebook.service;
 
 import com.br.ecommercebook.dto.CategoryDTO;
 import com.br.ecommercebook.entity.Category;
+import com.br.ecommercebook.exception.CategoryAlreadyExistsException;
+import com.br.ecommercebook.exception.CategoryNotFoundException;
 import com.br.ecommercebook.repository.CategoryRepository;
 import com.br.ecommercebook.vo.CategoryVO;
 import lombok.AllArgsConstructor;
@@ -15,8 +17,38 @@ public class CategoryService {
     private ModelMapper modelMapper;
 
     public CategoryVO create(CategoryDTO categoryDTO) {
-        var category = modelMapper.map(categoryDTO, Category.class);
-        categoryRepository.save(category);
-        return modelMapper.map(category, CategoryVO.class);
+        if(categoryDTO.getSubCategoryDTOList().isEmpty()) {
+            try {
+                findByName(categoryDTO);
+                throw new CategoryAlreadyExistsException();
+            } catch(CategoryNotFoundException e) {
+                var category = modelMapper.map(categoryDTO, Category.class);
+                categoryRepository.save(category);
+                return modelMapper.map(category, CategoryVO.class);
+            }
+        } else {
+            try {
+                var oldCategoryDTO = modelMapper.map(findByName(categoryDTO), CategoryDTO.class) ;
+                categoryDTO.getSubCategoryDTOList().forEach((subCategoryDTO -> {
+                    oldCategoryDTO.getSubCategoryDTOList().add(subCategoryDTO);
+                }));
+                var category = modelMapper.map(oldCategoryDTO, Category.class);
+                categoryRepository.save(category);
+                return modelMapper.map(category, CategoryVO.class);
+
+            } catch(CategoryNotFoundException e) {
+                var category = modelMapper.map(categoryDTO, Category.class);
+                categoryRepository.save(category);
+                return modelMapper.map(category, CategoryVO.class);
+            }
+        }
+    }
+
+    public CategoryVO findByName(CategoryDTO categoryDTO) {
+        var category = categoryRepository.findByName(categoryDTO.getName());
+        if (category.isEmpty()) {
+            throw new CategoryNotFoundException();
+        }
+        return modelMapper.map(category.get(), CategoryVO.class);
     }
 }
--- src/main/java/com/br/ecommercebook/service/SubCategoryService.java
+++ src/main/java/com/br/ecommercebook/service/SubCategoryService.java
@@ -1,4 +1,26 @@
 package com.br.ecommercebook.service;
 
+import com.br.ecommercebook.dto.CategoryDTO;
+import com.br.ecommercebook.entity.Category;
+import com.br.ecommercebook.entity.SubCategory;
+import com.br.ecommercebook.repository.SubCategoryRepository;
+import com.br.ecommercebook.vo.SubCategoryVO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class SubCategoryService {
-        }
+    private SubCategoryRepository subCategoryRepository;
+//
+//    public List<SubCategoryVO> create(CategoryDTO categoryDTO, Category category) {
+//        categoryDTO.getSubCategoryDTOList().forEach((subCategoryDTO) -> {
+//            category.set
+//            subCategoryRepository.save()
+//        });
+//    }
+//
+//    private Category updateCategorySubcategory(Long categoryId, SubCategory subCategory) {
+//
+//    }
+}
