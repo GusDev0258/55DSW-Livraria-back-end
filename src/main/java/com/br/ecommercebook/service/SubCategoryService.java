613cbb9ce98476a10af8c6c34e0ae7031ed329ea
LuizFJP <luizfjportela@gmail.com>
2023-08-24 21:59:59 -0300

    feat create sub categories

--- src/main/java/com/br/ecommercebook/service/SubCategoryService.java
+++ src/main/java/com/br/ecommercebook/service/SubCategoryService.java
@@ -1,26 +1,45 @@
 package com.br.ecommercebook.service;
 
 import com.br.ecommercebook.dto.CategoryDTO;
+import com.br.ecommercebook.dto.SubCategoryDTO;
 import com.br.ecommercebook.entity.Category;
 import com.br.ecommercebook.entity.SubCategory;
+import com.br.ecommercebook.exception.CategoryNotFoundException;
 import com.br.ecommercebook.repository.SubCategoryRepository;
+import com.br.ecommercebook.vo.CategoryVO;
 import com.br.ecommercebook.vo.SubCategoryVO;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class SubCategoryService {
-    private SubCategoryRepository subCategoryRepository;
+    private final SubCategoryRepository subCategoryRepository;
+    private final ModelMapper modelMapper;
 //
-//    public List<SubCategoryVO> create(CategoryDTO categoryDTO, Category category) {
-//        categoryDTO.getSubCategoryDTOList().forEach((subCategoryDTO) -> {
-//            category.set
-//            subCategoryRepository.save()
-//        });
+    public List<SubCategory> create(Category categoryEntity, CategoryDTO categoryDTO) {
+        var category = modelMapper.map(categoryDTO, Category.class);
+        List<SubCategory> subCategories = new ArrayList<>();
+        category.getSubCategory().forEach((subCategory -> {
+            subCategory.setCategory(categoryEntity);
+            subCategories.add(subCategoryRepository.save(subCategory));
+        }));
+        return subCategories;
+    }
+//
+//    private Category updateSubcategory(Long categoryId, CategoryDTO categoryDTO) {
+//
 //    }
-//
-//    private Category updateCategorySubcategory(Long categoryId, SubCategory subCategory) {
-//
+
+//    public SubCategoryVO findByName(SubCategoryDTO subCategoryDTO) {
+//        var subCategory = subCategoryRepository.findByName(subCategoryDTO.getName());
+//        if (subCategory.isEmpty()) {
+//            throw new CategoryNotFoundException();
+//        }
+//        return modelMapper.map(category.get(), CategoryVO.class);
 //    }
 }
