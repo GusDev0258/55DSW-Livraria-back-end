0aade5ab242719cc0c6c06883eba51e2652bae75
LuizFJP <luizfjportela@gmail.com>
2023-08-24 21:59:59 -0300

    feat create sub categories

--- src/main/java/com/br/ecommercebook/service/CategoryService.java
+++ src/main/java/com/br/ecommercebook/service/CategoryService.java
@@ -2,19 +2,25 @@ package com.br.ecommercebook.service;
 
 import com.br.ecommercebook.dto.CategoryDTO;
 import com.br.ecommercebook.entity.Category;
+import com.br.ecommercebook.entity.SubCategory;
 import com.br.ecommercebook.exception.CategoryAlreadyExistsException;
 import com.br.ecommercebook.exception.CategoryNotFoundException;
 import com.br.ecommercebook.repository.CategoryRepository;
 import com.br.ecommercebook.vo.CategoryVO;
+import com.br.ecommercebook.vo.SubCategoryVO;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class CategoryService {
     private CategoryRepository categoryRepository;
     private ModelMapper modelMapper;
+    private SubCategoryService subCategoryService;
 
     public CategoryVO create(CategoryDTO categoryDTO) {
         if(categoryDTO.getSubCategoryDTOList().isEmpty()) {
@@ -38,8 +44,14 @@ public class CategoryService {
 
             } catch(CategoryNotFoundException e) {
                 var category = modelMapper.map(categoryDTO, Category.class);
-                categoryRepository.save(category);
-                return modelMapper.map(category, CategoryVO.class);
+                category.setSubCategory(new ArrayList<>());
+                var categoryEntity = categoryRepository.save(category);
+                var subCategories = subCategoryService.create(categoryEntity, categoryDTO);
+                categoryEntity.setSubCategory(subCategories);
+                categoryRepository.save(categoryEntity);
+                var categoryVO = findByName(categoryDTO);
+                categoryVO.setSubCategoriesVO(subCategories.stream().map((subCategory -> modelMapper.map(subCategory, SubCategoryVO.class))).collect(Collectors.toList()));
+                return categoryVO;
             }
         }
     }
@@ -49,6 +61,7 @@ public class CategoryService {
         if (category.isEmpty()) {
             throw new CategoryNotFoundException();
         }
+        var a = category.get();
         return modelMapper.map(category.get(), CategoryVO.class);
     }
 }
