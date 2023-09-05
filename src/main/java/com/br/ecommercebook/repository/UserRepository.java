741e7be447029e1657bb081e7a49b8a6d377a290
gusdev0258 <16326176760@edu.udesc.br>
2023-09-05 11:32:36 -0300

    Removed unused import from userRepository

--- src/main/java/com/br/ecommercebook/repository/UserRepository.java
+++ src/main/java/com/br/ecommercebook/repository/UserRepository.java
@@ -1,12 +1,13 @@
 package com.br.ecommercebook.repository;
 
 import com.br.ecommercebook.entity.User;
-import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     public Optional<User> findByEmail(String email);
 }
