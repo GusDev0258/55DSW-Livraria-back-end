6fb2417c25e567a423633c7d0ee3cf29c422c471
gusdev0258 <16326176760@edu.udesc.br>
2023-09-05 19:54:49 -0300

    feat: implemented extractUserEmail

--- src/main/java/com/br/ecommercebook/service/JwtService.java
+++ src/main/java/com/br/ecommercebook/service/JwtService.java
@@ -17,7 +17,7 @@ public class JwtService {
   private static final String SECRET_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTY5Mzk1MTc1NywiaWF0IjoxNjkzOTUxNzU3fQ.Ch2pcO68uo5LQN9hvu33WyE_h76JPI2A7023fAenBTU";
 
   public String extractUserEmail(String jwtToken) {
-    return null;
+    return extractClaim(jwtToken, Claims::getSubject);
   }
 
   public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
