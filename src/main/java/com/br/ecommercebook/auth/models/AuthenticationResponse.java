1123e40056637ab8a0693c93ab9d78d9cc61131c
gusdev0258 <16326176760@edu.udesc.br>
2023-09-05 21:49:14 -0300

    feat: create user register and authentication

--- src/main/java/com/br/ecommercebook/auth/models/AuthenticationResponse.java
+++ src/main/java/com/br/ecommercebook/auth/models/AuthenticationResponse.java
@@ -1,10 +1,14 @@
-package com.br.ecommercebook.vo;
+package com.br.ecommercebook.auth.models;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Data
+@Builder
+@NoArgsConstructor
 public class AuthenticationResponse {
     String token;
 }
--- src/main/java/com/br/ecommercebook/entity/User.java
+++ src/main/java/com/br/ecommercebook/entity/User.java
@@ -27,7 +27,8 @@ public class User implements UserDetails{
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name="user_id")
     private Long id;
-    private String name;
+    private String firstname;
+    private String lastname;
     private String phone;
     private String email;
     private String password;
