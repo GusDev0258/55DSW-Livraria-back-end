cc71cb986e48781c2af16e9b334f08851b22243e
gusdev0258 <16326176760@edu.udesc.br>
2023-09-05 21:50:32 -0300

    feat: removed modelMapper bean from Beans and placed on ApplicationConfiguration

--- src/main/java/com/br/ecommercebook/configuration/ApplicationConfiguration.java
+++ src/main/java/com/br/ecommercebook/configuration/ApplicationConfiguration.java
@@ -1,5 +1,6 @@
 package com.br.ecommercebook.configuration;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -43,4 +44,9 @@ public class ApplicationConfiguration {
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
+
+  @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
